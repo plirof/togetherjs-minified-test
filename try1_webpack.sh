c1ebbda8340b64ce7ed974d1516ccb1e4b0c9a59
aaa <aaa@aaa.com>
2020-04-02 16:02:14 +0300

    200331b added webpack-cli node-modules local

--- try1_webpack.sh
+++ try1_webpack.sh
@@ -1,3 +1,3 @@
 echo "https://stackoverflow.com/questions/53153935/webpack-split-chunks-put-everything-in-one-file"
 
-./node_modules/.bin/webpack app/entry.js dist/output.js
+./node_modules/.bin/webpack togetherjs-min.js dist/output.js
