82637238fd89ccfcdeae86675977c386a24aef2a
aaa <aaa@aaa.com>
2020-04-02 16:28:30 +0300

    200331c

--- try1_webpack.sh
+++ try1_webpack.sh
@@ -1,3 +1,4 @@
 echo "https://stackoverflow.com/questions/53153935/webpack-split-chunks-put-everything-in-one-file"
 
-./node_modules/.bin/webpack togetherjs-min.js dist/output.js
+#./node_modules/.bin/webpack togetherjs-min.js dist/output.js
+./node_modules/.bin/webpack togetherjs-min.js
